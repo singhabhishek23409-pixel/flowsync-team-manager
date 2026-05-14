b88ce9746b95fe6263c883842252ecb1f9cf906d
Anshul Pratap Singh <anshulpratap2349@gmail.com>
2026-05-15 03:20:20 +0530

    Fix Railway allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT || 4173,
-    allowedHosts: ["all"],
+    allowedHosts: [
+      "flowsync-production-adb0.up.railway.app",
+    ],
   },
 });
